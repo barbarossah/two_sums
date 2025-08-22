b093db26afd21e99f13bc574a3101f2271995189
Ismoil <ismoil997@seznam.cz>
2025-08-22 13:32:15 +0500

    init: two_sums

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1,2,5,6,7,8};
+        int sum = 11;
+
+        for(int i = 0; i < arr.length; i++){
+            for(int j = i+1; j < arr.length; j++){
+                if(arr[i]+arr[j] == sum){
+                    System.out.println(arr[i]+" "+arr[j]);
+                }
+            }
+        }
+    }
+}
